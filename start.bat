ae44f332aedbc3c530dbe20f3f224d6c661187e7
dankl <dan@klueter.us>
2026-06-21 16:34:19 -0400

    minor fixes

--- start.bat
+++ start.bat
@@ -3,4 +3,4 @@ cd /d "%~dp0"
 pip install -r requirements.txt -q
 start python app.py
 timeout /t 2 /nobreak >nul
-start firefox http://localhost:5001
+start firefox http://localhost:5000
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd ~/golf_handicap
+cd /Users/dankl/golf_handicap
 pip3 install -r requirements.txt -q
 python3 app.py &
 sleep 2
-open -a Firefox http://localhost:5001
+open -a Firefox http://localhost:5000
