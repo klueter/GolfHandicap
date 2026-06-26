2e87f7005b1f3e0ccd791109b510f45f45046b4d
dankl <dan@klueter.us>
2026-06-26 15:22:50 -0400

    Create macstart.sh

---
+++ golf_handicap/macstart.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /Users/dankl/Documents/GitHub/GolfHandicap/golf_handicap
+
+# Kill any existing app.py process
+pkill -f "python3 app.py" 2>/dev/null
+sleep 1
+
+pip3 install -r requirements.txt -q
+python3 app.py &
+sleep 2
+open -a Firefox http://localhost:5000
