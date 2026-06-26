2208e8e7f9ea6803d1aacfac2e5be3072ce95521
dankl <dan@klueter.us>
2026-06-25 20:53:30 -0400

    placement

--- start.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-pip install -r requirements.txt -q
-start python app.py
-timeout /t 2 /nobreak >nul
-start firefox http://localhost:5000
--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /Users/dankl/golf_handicap
-pip3 install -r requirements.txt -q
-python3 app.py &
-sleep 2
-open -a Firefox http://localhost:5000
